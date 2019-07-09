2b88a0243560a6c6cd83fafccb8447a050551122
Marcin Środa <msroda@gbox.pl>
2019-07-09 08:43:46 +0200

    Lifecycle for created app without connectivity check

--- scarlet-lifecycle-android/src/main/java/com/tinder/scarlet/lifecycle/android/LifecycleOwnerCreatedLifecycle.kt
+++ scarlet-lifecycle-android/src/main/java/com/tinder/scarlet/lifecycle/android/LifecycleOwnerCreatedLifecycle.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import com.tinder.scarlet.Lifecycle
+import com.tinder.scarlet.LifecycleState
 import com.tinder.scarlet.lifecycle.LifecycleRegistry
 
 internal class LifecycleOwnerCreatedLifecycle(
@@ -20,8 +21,13 @@ internal class LifecycleOwnerCreatedLifecycle(
     }
 
     private inner class ALifecycleObserver : LifecycleObserver {
+        @OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_CREATE)
+        fun onCreate() {
+            lifecycleRegistry.onNext(LifecycleState.Started)
+        }
         @OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_DESTROY)
         fun onDestroy() {
+            lifecycleRegistry.onNext(LifecycleState.Stopped)
             lifecycleRegistry.onComplete()
         }
     }
